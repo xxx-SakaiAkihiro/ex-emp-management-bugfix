28cc1f62c906f55b67d5e692d571ed6c29c82be6
SakaiAkihiro <akihiro.sakai@rakus-partners.co.jp>
2019-11-14 15:30:18 +0900

    (1-2)初級 入力値エラー処理

--- src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
@@ -1,5 +1,7 @@
 package jp.co.sample.emp_management.form;
 
+import javax.validation.constraints.NotBlank;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,33 +10,43 @@ package jp.co.sample.emp_management.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message="名前の入力は必須です")
 	private String name;
 	/** メールアドレス */
+	@NotBlank(message="メールアドレスの入力は必須です")
 	private String mailAddress;
 	/** パスワード */
+	@NotBlank(message="パスワードの入力は必須です")
 	private String password;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getMailAddress() {
 		return mailAddress;
 	}
+
 	public void setMailAddress(String mailAddress) {
 		this.mailAddress = mailAddress;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	@Override
 	public String toString() {
 		return "InsertAdministratorForm [name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
 				+ "]";
 	}
-	
+
 }
